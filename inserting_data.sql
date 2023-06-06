1a8e5304b086bd54283ca0af568f4f03e46e005a
Severino Ribeiro Neto <62965664+SeverinoRibNeto@users.noreply.github.com>
2023-06-06 11:21:36 -0300

    insert data into table

---
+++ inserting_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO aluno (nome,
+				   cpf,
+				   observacao,
+				   idade,
+				   valor_matricula,
+				   altura,
+				   ativo,
+				   data_nascimento,
+				   hora_aula,
+				   matriculado_em) 
+VALUES 
+                ('Severino', 
+                '01234567890',
+                'lorem Laborum consectetur culpa exercitation mollit.',
+                30,
+                250.00,
+                1.75,
+                TRUE,
+                '1996-01-29',
+                '17:30:00' ,
+                '2023-06-06 10:00:00');
